1359210da2f4163f08310b7a82c63e7f469ce62e
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-22 22:48:38 -0300

    Desafio RocketSeat

--- agenda_rocket.py
+++ agenda_rocket.py
@@ -1,9 +1,19 @@
-from
+from validate_email import validate_email
 def adicionar_contatos(contatos, nome, telefone, email):
-  contato = {"nome": nome, "telefone": telefone, "email": email, "favorito": False}
-  contatos.append(contato)
-  print(f"\nContato adicionado: {nome}")
-  return
+  if len(nome) < 4:
+    print("\nO nome tem que conter ao menos 4 dígitos. Contato não foi salvo.")
+    return
+  elif len(telefone) < 8:
+    print("\nTelefone tem que conter ao menos 8 dígitos. Contato não foi salvo.")
+    return
+  elif not validate_email(email):
+    print("\nEste e-mail é inválido. Contato não sera salvo.")
+    return
+  else: 
+    contato = {"nome": nome, "telefone": telefone, "email": email, "favorito": False}
+    contatos.append(contato)
+    print(f"\nContato adicionado: {nome}")
+    return
 
 def visualizar_contatos(contatos):
   print("Contatos: ")
@@ -33,9 +43,9 @@ while True:
 
   if escolha == "1":
     print("\nCriando contato.")
-    nome_contato = str(input("\nAdicione o nome do contato: "))
-    telefone_contato = str(input("\nAdicione o telefone do contato: "))
-    email_contato = str(input("\nAdicione o email do contato: "))
+    nome_contato = str(input("\n*OBRIGATÓRIO* Adicione o nome do contato: "))
+    telefone_contato = str(input("\n*OBRIGATÓRIO* Adicione o telefone do contato: "))
+    email_contato = str(input("\n*OBRIGATÓRIO* Adicione o email do contato: "))
     adicionar_contatos(contatos, nome_contato, telefone_contato, email_contato)
   elif escolha == "2":
     print("\n Lista de contatos.")
